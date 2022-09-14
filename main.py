968fc88140d2e397effd6ef9505964fbe5db35db
SkywalkerZ <abraarshk98@gmail.com>
2022-09-14 21:41:02 +0530

    first blood

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import random
+
+#split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+#random picker
+length=len(names)
+random_generator=random.randint(0,length-1)
+print(names[random_generator] + " is going to buy the meal today!")
