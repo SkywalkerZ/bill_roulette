f6f7f40387b0711e0c38e54a77bdf3f1a6b10288
SkywalkerZ <abraarshk98@gmail.com>
2022-09-20 16:33:30 +0530

    Version 2

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
 
-#split string method
-names_string = input("Give me everybody's names, separated by a comma.: \n")
-names = names_string.split(", ")
+# #split string method
+# names_string = input("Give me everybody's names, separated by a comma.: \n")
+# names = names_string.split(", ")
 
-#random picker
-length=len(names)
-random_generator=random.randint(0,length-1)
-print("\n" + names[random_generator] + " is going to buy the meal today!")
+# #random picker
+# length=len(names)
+# random_generator=random.randint(0,length-1)
+# print("\n" + names[random_generator] + " is going to buy the meal today!")
+
+names = input("Give me everybody's name, separated by a ', ' (comma): \n").lower().split(", ")
+final_index = random.randint(0, len(names))
+final_person = names[final_index]
+
+print(f"{final_person.title()} is going to buy the meal today!")
